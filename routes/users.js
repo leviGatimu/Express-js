a021ee72a7b2a3fe2b3122389c88254bc7aa44c1
leviGatimu <getmorelev@gmail.com>
2026-02-10 22:59:45 +0200

    fixes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from 'experss';
 
-const router = express.Router();
+const routes = express.Routes();
 
 const users = [
     {
@@ -15,18 +15,14 @@ const users = [
     }
 ]
 
-//All routes here start from /users , before it was /users/users
-router.get('/', (req, res) => {
+routes.get('/', (req, res) => {
     console.log(users);
     res.send(users);
 });
 
-router.post('/', (req , res) => {
+routes.post('/', (req, res) => {
+    const user = req.body;
+    res.send(`User succesfully inputed: ${user}`);
+    console.log(user);
+});
 
-    const user= req.body;
-    users.push(user);
-
-    res.send(`User with the name ${user.firstName} was added to the database!`);
-})
-
-export default router;
