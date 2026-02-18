ffb99fd61d6edb28a9f8a51df88a60c965b1e8d7
leviGatimu <getmorelev@gmail.com>
2026-02-18 11:31:33 +0200

    Updates: 2026-02-18T09:31:30.641Z

---
+++ routes/cars.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const router = express.Router();
+
+const cars = [
+  { id: 1, brand: "Toyota", country: "Japan" },
+  { id: 2, brand: "BMW", country: "Germany" },
+  { id: 3, brand: "Ford", country: "USA" },
+  { id: 4, brand: "Mercedes-Benz", country: "Germany" },
+  { id: 5, brand: "Hyundai", country: "South Korea" },
+  { id: 6, brand: "Tesla", country: "USA" },
+  { id: 7, brand: "Honda", country: "Japan" },
+  { id: 8, brand: "Volkswagen", country: "Germany" }
+];
+
+router.post('/', (req, res)=>{
+    const car = req.body;
+    cars.push(car);
+    res.send(`The car ${car.brand} was successfully added`);
+});
+
+router.get('/', (req, res)=> {
+    res.send(cars);
+});
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const car = cars.find(c => c.id === id);
+    if(!car){
+        return res.status(404).send("Car is not found");
+    }
+    res.send(car);
+});
+
+
+export default router;
