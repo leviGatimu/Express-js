18171d60a7b7e4b0a4fd925c6976144cf8f157ca
leviGatimu <getmorelev@gmail.com>
2026-02-19 17:38:07 +0200

    Updates: 2026-02-19T15:38:05.006Z

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import todoRoute from './routes/todo.js';
+import patientRoute from './routes/patients.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use('/todo', todoRoute);
+app.use('/patients', patientRoute);
 
 app.get("/", (req, res)=> {
     res.send("Welcome to homepage dp /todo to visit todos");
